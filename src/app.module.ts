86c0a03f483f9cf97fb10bca521e19be79645104
Jose Ramon Alvarez <joraalgo@gmail.com>
2024-03-25 13:57:41 +0100

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,6 +20,8 @@ import { MensajesService } from './mensajes/mensajes.service';
       synchronize: true,
     }),
     TypeOrmModule.forFeature([Mensaje]),
+    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ cache: true }),
   ],
   controllers: [AppController, MensajesController],
   providers: [AppService, MensajesService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getTest(): string {
+    const a = process.env.KEY_TEST;
+    return `Test ${a} Ok!`;
+  }
 }
